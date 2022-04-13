95adeb2c3299a43fd4b65485e4cb1b33cde60f22
Eli T <eliyhaoo@gmail.com>
2022-04-13 19:40:38 +0300

    finsihed thirdPhase

--- js/meme-controller.js
+++ js/meme-controller.js
@@ -1,87 +1,107 @@
-'use strict'
+"use strict";
 
-let gElCanvas
-let gCtx
+let gElCanvas;
+let gCtx;
 
-function renderMeme(id,idx) {
-    const meme = getMeme(id)
-    drawImgFromLocal(meme,idx)
-      
+function renderMeme(id, idx) {
+  const meme = getMeme(id);
+  drawImgFromLocal(meme, idx);
 }
 
-function initCanvas(){
-    gElCanvas = document.querySelector('#canvas')
-    gCtx = gElCanvas.getContext("2d")
-    window.addEventListener('resize',resizeCanvas)
-    // resizeCanvas()  
+function initCanvas() {
+  gElCanvas = document.querySelector("#canvas");
+  gCtx = gElCanvas.getContext("2d");
+  window.addEventListener("resize", resizeCanvas);
+  // resizeCanvas()
 }
-
-
 
 ///////////// OnClickEvents /////////////////
-function onTextInput(txt){
-    console.log('changed');
-    setLineTxt(txt)
-    renderMeme()
+function onTextInput(txt) {
+  console.log("changed");
+  setLineTxt(txt);
+  renderMeme();
 }
 
-function onChangeColor(val){
-    console.log('val',val);
-    setColor(val)
-    renderMeme()
+function onChangeColor(val) {
+  console.log("val", val);
+  setColor(val);
+  renderMeme();
 }
 
 function onFontSizeChange(diff) {
-    changeFontSize(diff)
-    renderMeme()
+  changeFontSize(diff);
+  renderMeme();
 }
 
-
-
+function onSwitchLines() {
+  setSelectedLine();
+  renderMeme();
+}
 
 //////////////\\\\\\\DRAW FUNCTINOS ///////////\\\\\\\\\\\\\
-function drawText(x,y,txt,size,align,color) {
-    console.log(txt);
-    gCtx.beginPath()
-    gCtx.textBaseline = 'middle' //read more about index for row
-    gCtx.textAlign= align
-    gCtx.fillStyle = color
-    gCtx.font = `${size}px impact`
-    gCtx.fillText(txt, x, y);
+function drawText(x, y, txt, size, align, color) {
+  gCtx.beginPath();
+  gCtx.textBaseline = "middle";
+  gCtx.textAlign = align;
+  gCtx.fillStyle = color;
+  gCtx.font = `${size}px impact`;
+  gCtx.fillText(txt, x, y);
+  gCtx.lineWidth = 2;
+  gCtx.strokeStyle = "black";
+  gCtx.strokeText(txt, x, y);
 }
 
-function drawRect(x,y,size){
-
-    gCtx.beginPath()
-    gCtx.rect(x,y,gElCanvas.width-size,size)
-    gCtx.lineWidth= 3
-    gCtx.strokeStyle = 'white'
-    gCtx.stroke()
+function drawRect(x, y, size) {
+  let gradient = gCtx.createLinearGradient(100, 60, 240, 40);
+  gradient.addColorStop("0", "magenta");
+  gradient.addColorStop("0.5", "blue");
+  gradient.addColorStop("1.0", "orange");
+  gCtx.beginPath();
+  gCtx.rect(x, y, gElCanvas.width - size, size);
+  gCtx.lineWidth = 3;
+  gCtx.strokeStyle = gradient;
+  gCtx.stroke();
 }
 
+function drawImgFromLocal(meme, idx = 0) {
+  const { lines, selectedImgId } = meme;
+  let img = new Image();
+  img.src = `meme-imgs/${selectedImgId}.jpg`;
+  img.onload = () => {
+    gCtx.drawImage(img, 0, 0, gElCanvas.width, gElCanvas.height);
+    console.log("lines", lines);
+    let count = 0;
+    lines.map((line, idx) => {
+      const { txt, size, align, color, isSelected } = line;
+      const height = idx === 0 ? size : gElCanvas.height - size;
 
-function drawImgFromLocal(meme,idx =0){
-    const {lines,selectedImgId} = meme
-    let img = new Image()
-    img.src = `meme-imgs/${selectedImgId}.jpg`
-    img.onload= ()=>{
-        gCtx.drawImage(img,0,0,gElCanvas.width,gElCanvas.height)
-        const {txt,size,align,color} = lines[idx]
-   
-        drawText(gElCanvas.width/2,size,txt,size,align,color)
-        drawRect(size/2,size/2,size)
-        
-    } 
+      drawText(gElCanvas.width / 2, height, txt, size, align, color);
+      // drawText(gElCanvas.width/2,size,txt,size,align,color)
+      //    drawRect(size / 2, size / 2, size);// first row
+      //    drawRect(size / 2, gElCanvas.height -size*1.5 , size);
+
+      if (isSelected) {
+        switch (meme.selectedLineIdx) {
+          case 0:
+            drawRect(size / 2, size / 2, size);
+            break;
+          case 1:
+            drawRect(size / 2, gElCanvas.height - size * 1.5, size);
+            break;
+        }
+      }
+    });
+
+    // const {txt,size,align,color} = lines[idx]
+
+    // drawText(gElCanvas.width/2,size,txt,size,align,color)
+    // drawRect(size/2,size/2,size)
+  };
 }
 
-function resizeCanvas(){
-    //come back to fix responsivity to all pics. 
-    // const elContainer = document.querySelector('.canvas-container')
-    // gElCanvas.width = elContainer.offsetWidth -10
-    // gElCanvas.height = elContainer.offsetHeight -10 
+function resizeCanvas() {
+  //come back to fix responsivity to all pics.
+  // const elContainer = document.querySelector('.canvas-container')
+  // gElCanvas.width = elContainer.offsetWidth -10
+  // gElCanvas.height = elContainer.offsetHeight -10
 }
-
-
-
-
-
--- js/meme-service.js
+++ js/meme-service.js
@@ -2,30 +2,48 @@
 
 let gMeme 
 
-
-
-
 function getMeme(id){
     if(!gMeme){
         gMeme= _createMeme(id)
+        console.log('sourcer',gMeme);
     }
   return gMeme
 }
 
 
 function setLineTxt(txt){ ///// REMEMBER TO MAKE THE LINE CHANGABLE
-    gMeme.lines[0].txt =txt
+    gMeme.lines[gMeme.selectedLineIdx].txt =txt
    
 }
 
 function setColor(color) { ///// REMEMBER TO MAKE THE LINE CHANGABLE
     console.log('are you crazy?');
-    gMeme.lines[0].color = color
+    gMeme.lines[gMeme.selectedLineIdx].color = color
 }
 
 function changeFontSize(diff){
-    gMeme.lines[0].size += +diff
+    gMeme.lines[gMeme.selectedLineIdx].size += +diff
 }
+
+function setSelectedLine(){ 
+    console.log('befpre',gMeme.selectedLineIdx);
+    gMeme.lines[gMeme.selectedLineIdx].isSelected= false
+    if ( gMeme.selectedLineIdx  === gMeme.lines.length-1) gMeme.selectedLineIdx--
+    else gMeme.selectedLineIdx++
+    console.log('after',gMeme.selectedLineIdx);
+    gMeme.lines[gMeme.selectedLineIdx].isSelected= true
+    
+}
+
+
+// function addLine(){
+//     gMeme.lines.push({
+//         txt: 'text goes here',
+//             size: 50,
+//             align: 'center',
+//             color: 'white'
+//     })
+// }
 
 
 
@@ -33,11 +51,21 @@ function _createMeme(id){
     return  {
         selectedImgId: id,
         selectedLineIdx:0,
-        lines: [{
+        lines: [
+            {
             txt: 'text goes here',
             size: 50,
             align: 'center',
-            color: 'white'
-        }]
+            color: 'white',
+            isSelected: true
+        },
+            {
+            txt: 'text goes here',
+            size: 50,
+            align: 'center',
+            color: 'white',
+            isSelected: false
+        },
+    ]
     }
 }
